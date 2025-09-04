3d4599c168d59c333c8184b3488c740c47b110c9
noname 00X67 <00x67g@gmail.com>
2025-09-04 20:54:53 +0000

    sur les pages produits de la boutique j'ai deux fois le footer

--- src/app/store/[productId]/page.tsx
+++ src/app/store/[productId]/page.tsx
@@ -2,8 +2,6 @@
 import { notFound } from 'next/navigation';
 import { products, type Product } from '@/lib/products';
 import ProductClient from './client';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
 
 export function generateStaticParams() {
   return products.map((product) => ({
@@ -27,13 +25,9 @@ const ProductPage = async ({ params: { productId } }: { params: { productId: str
   ).slice(0, 4);
 
   return (
-    <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1 bg-background/95">
-            <ProductClient product={product} relatedProducts={relatedProducts} />
-        </main>
-        <Footer />
-    </div>
+    <main className="flex-1 bg-background/95">
+        <ProductClient product={product} relatedProducts={relatedProducts} />
+    </main>
   );
 }
 
