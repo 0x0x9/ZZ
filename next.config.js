67702d6b4d3130b4f5b43c122d6e18668216df5d
noname 00X67 <00x67g@gmail.com>
2025-09-20 14:30:14 +0000

    ou alors je télécharge les images et je les mets dans un dossier images

--- next.config.js
+++ next.config.js
@@ -29,12 +29,6 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
   devIndicators: {
